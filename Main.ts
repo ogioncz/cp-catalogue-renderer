75e6e270232e128d4c35e44d491fcfa041fb1a2a
Jan Tojnar <jtojnar@gmail.com>
2015-05-08 20:48:13 +0200

    refactoring: added type annotations for better intellisense

--- Main.ts
+++ Main.ts
@@ -1,45 +1,47 @@
 import {Renderer} from './Renderer';
 import {Utils} from './Utils';
 import {WikitextGenerator} from './WikitextGenerator';
+import {WikitextTemplate} from './WikitextTemplate';
 import {ItemGenerator} from './generators/ItemGenerator';
 import {FurnitureGenerator} from './generators/FurnitureGenerator';
 import {EnItemTemplate} from './templates/EnItemTemplate';
 import {EnFurnitureTemplate} from './templates/EnFurnitureTemplate';
 import {PtFurnitureTemplate} from './templates/PtFurnitureTemplate';
 
+var langSelect : HTMLSelectElement = <HTMLSelectElement> document.getElementById('lang');
+
 document.getElementById('render').addEventListener('click', function renderCatalogue(e) {
-	let container = document.createElement('div');
+	let container : HTMLElement = document.createElement('div');
 	document.body.appendChild(container);
 
-	let lang = document.getElementById('lang');
-	lang = lang.options[lang.selectedIndex].value;
+	let lang = langSelect.options[langSelect.selectedIndex].value;
 
-	let catalogue = document.getElementById('catalogue');
-	catalogue = catalogue.options[catalogue.selectedIndex].value;
+	let catalogueSelect : HTMLSelectElement = <HTMLSelectElement> document.getElementById('catalogue');	
+	let catalogue = catalogueSelect.options[catalogueSelect.selectedIndex].value;
 
 	let renderer = new Renderer(container, 'http://media8.clubpenguin.com/mobile/cp-mobile-ui/clubpenguin_v1_6/' + Utils.full_langs[lang], catalogue);
 });
 
 document.getElementById('item').addEventListener('click', function renderCatalogue(e) {
-	if (!document.getElementById('itemid').checkValidity()) {
+	let itemidEntry : HTMLInputElement = <HTMLInputElement> document.getElementById('itemid');
+	if (!itemidEntry.checkValidity()) {
 		alert('Please enter a valid ID.');
 		return;
 	}
 
-	let container = document.createElement('pre');
+	let container : HTMLElement = document.createElement('pre');
 	document.body.appendChild(container);
 
 	container.innerHTML = '<div class="loading"></div>Loading…';
 
-	let lang = document.getElementById('lang');
-	lang = lang.options[lang.selectedIndex].value;
+	let lang = langSelect.options[langSelect.selectedIndex].value;
 
-	let itemtype = document.getElementById('itemtype');
-	itemtype = itemtype.options[itemtype.selectedIndex].value;
+	let itemtypeSelect : HTMLSelectElement = <HTMLSelectElement> document.getElementById('itemtype');
+	let itemtype = itemtypeSelect.options[itemtypeSelect.selectedIndex].value;
 
-	let itemid = parseInt(document.getElementById('itemid').value);
+	let itemid = parseInt(itemidEntry.value);
 
-	var template, generatorClass : WikitextGenerator;
+	var template : WikitextTemplate, generatorClass;
 	if (itemtype === 'item') {
 		generatorClass = ItemGenerator;
 		if (lang === 'en') {
@@ -55,7 +57,7 @@ document.getElementById('item').addEventListener('click', function renderCatalog
 	}
 
 	if (template) {
-		let generator = new generatorClass(template);
+		let generator : WikitextGenerator = new generatorClass(template);
 		generator.render(itemid).then(function(result) {
 			container.innerHTML = result;
 			Utils.eachChild(container, 'input[type="radio"]', function(el) {
@@ -65,13 +67,14 @@ document.getElementById('item').addEventListener('click', function renderCatalog
 				el.addEventListener('change', function(e) {
 					Utils.eachChild(container, '[data-name="' + this.name + '"]', (child) => child.style.display = 'none');
 					Utils.eachChild(container, '[data-name="' + this.name + '"][data-value="' + this.value + '"]', (child) => child.style.display = 'inline');
-				})
+				});
 			});
 		}).catch(function(msg) {
 			container.textContent = msg;
 			alert(msg);
 		});
 	} else {
+		container.parentNode.removeChild(container);
 		alert('Requested language/type combination isn’t yet supported.');
 	}
 });
