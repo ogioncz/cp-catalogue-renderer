f96ad317f08e66c1927c6e0aa79bbbb1c610da3a
Jan Tojnar <jtojnar@gmail.com>
2015-06-10 01:23:05 +0200

    copying: don't deselect in case copy is blocked

--- Main.ts
+++ Main.ts
@@ -142,10 +142,12 @@ document.addEventListener('click', function(e) {
 			var successful = document.execCommand('copy');
 			var msg = successful ? 'successful' : 'unsuccessful';
 			console.log('Copy email command was ' + msg);
+			if (successful) {
+				window.getSelection().removeAllRanges();				
+			}
 		} catch (err) {
 			console.log('Oops, unable to copy');
 		}
-		window.getSelection().removeAllRanges();
 
 		e.preventDefault();
 	}
